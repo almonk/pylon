4bba4806d22299efd350cec3b579c3b7353c7388
Alasdair Monk <alasdair.monk@gmail.com>
2019-06-14 12:14:54 +0100

    Setup for gulp

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ var atImport = require("postcss-import");
 var cssnano = require("cssnano");
 var cssvariables = require("postcss-css-variables");
 
-gulp.task("pylon", function() {
+gulp.task("buildCss", function() {
   var plugins = [atImport, cssnano, cssvariables];
 
   return gulp
@@ -14,3 +14,12 @@ gulp.task("pylon", function() {
     .pipe(gulp.dest("./css"))
     .pipe(gulp.dest("./docs/pylon"));
 });
+
+
+gulp.task("buildJs", function() {
+  return gulp
+    .src("./js/pylon.js")
+    .pipe(gulp.dest("./js"))
+});
+
+gulp.task("pylon", ["cbuildCssss", "buildJs"]);
